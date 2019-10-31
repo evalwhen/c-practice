e202954b8f9b56d5fd227be97998b63149e9af29
HuJianfeng <huzi.master@outlook.com>
2019-10-31 22:26:38 +0800

    fix: path.

--- bitmap/bitmap.c
+++ bitmap/bitmap.c
@@ -1,11 +1,6 @@
 #include "bitmap.h"
 
-#define BITSPERWORD  32
-#define SHIFT  5
-#define MASK  0x1F
-#define N  10000000
-
-int bitmap[1 + N/BITSPERWORD];
+static int bitmap[1 + N/BITSPERWORD];
 
 // i >> SHIFT == (floor i (expt 2 SHIFT))
 // i & MASK == (rem i (expt 2 SHIFT))
--- bitmap/bitmap.h
+++ bitmap/bitmap.h
@@ -1,6 +1,11 @@
 #ifndef BITMAP_H__
 #define BITMAP_H__
 
+#define BITSPERWORD  32
+#define SHIFT  5
+#define MASK  0x1F
+#define N  10000000
+
 void set(int i); // add element
 void clr(int i); // remove element
 int test(int i); // check that the element exists
