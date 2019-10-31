f7795388ce6c1eb8e66757156c56c42f9d02db87
HuJianfeng <huzi.master@outlook.com>
2019-10-31 22:34:46 +0800

    fix: remove bin.

--- sortfile/main.c
+++ sortfile/main.c
@@ -5,28 +5,28 @@
 #include <string.h>
 
 int main() {
-  FILE* f = fopen("rand_no_dup_int.txt", "r+");
+    FILE* f = fopen("rand_no_dup_int.txt", "r+");
     FILE* f2 = fopen("sort_rand_no_dup_int.txt", "w");
 
 
-  uint32_t i, total = 0;
-  char *line = NULL;
-  size_t len = 0;
+    uint32_t i, total = 0;
+    char *line = NULL;
+    size_t len = 0;
 
 
-  while(getline(&line, &len, f) != -1) {
-    int i = strtoimax(line, NULL, 10);
-    set(i);
-    total++;
-  }
-
-  for (i = 0; i < N; i++) {
-    if (test(i) != 0 ) {
-      fprintf(f2, "%d\n", i);
+    while(getline(&line, &len, f) != -1) {
+        int i = strtoimax(line, NULL, 10);
+        set(i);
+        total++;
     }
-  }
 
-  free(line);
-  fclose(f);
-  return 0;
+    for (i = 0; i < N; i++) {
+        if (test(i) != 0 ) {
+            fprintf(f2, "%d\n", i);
+        }
+    }
+
+    free(line);
+    fclose(f);
+    return 0;
 }
